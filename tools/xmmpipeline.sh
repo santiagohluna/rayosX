f32e946d27e2a37d5f5cbad272d6fea5e01ff8e1
Santiago Luna <santiago.luna@unahur.edu.ar>
2025-02-14 14:44:07 -0300

    Se continuó la modificación del script, pero aún faltan mejoras para que funcione

--- tools/xmmpipeline.sh
+++ tools/xmmpipeline.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+clear
+
 echo -e '\n============'
 echo -e 'XMM Pipeline'
 echo -e '============'
@@ -46,20 +48,25 @@ then
         printf "$((i+1)). ObsID: %s" "${obs[$i]}"
         i=$((i+1))
     done
-    printf "\nIngrese el índice identificador de la observación o el ObdId: "
+    printf "\nIngrese el índice identificador de la observación: "
     read -e id
     obsid=$(printf %s "${obs[$((id-1))]}")
 else
     obsid=$1
 fi
 
-printf "\nEl ID de la observación seleccionada es: %s.\n" "$obsid"
+# Almacenar la ruta hacia los archivos con las observaciones a procesar.
+indir=/home/shluna/Proyectos/rayosX/data/obs/xmm/$obsid/ODF
+
+# Apuntar la variable `SAS_ODF` a la carpeta donde se encuentran las observaciones
+export SAS_ODF=${indir}
+
+echo "Variable SAS_ODF: ${SAS_ODF}."
 
 # Cadena para el nombre de la carpeta de salida y log.
 STAMP="obsID_"$obsid"_"$(date +'d%Y%m%d_t%H%M%S')
 
 # Crear la carpeta donde se van a almacenar los archivos que resultan de la reducción.
-
 outdir=$STAMP
 
 if [ ! -d $outdir ]; then
@@ -68,85 +75,125 @@ fi
 
 cd ${outdir}
 
+echo -e "\nCarpeta actual: $(pwd)"
+
 # Crear el archivo log
-LOG_FILE=$STAMP"xmmpipeline_log.txt"
+LOG_FILE=$STAMP"_xmmpipeline_log.txt"
 
-echo '=======================' >> $LOG_FILE
-echo 'XMM Pipeline - Log file' >> $LOG_FILE
-echo '=======================' >> $LOG_FILE
+# Lista de comandos a ejecutar
+COMMANDS=(
+    # Iniciar HEASoft
+    "heainit"
+    # Iniciar SAS
+    "sasinit"
+    #
+    # Reprocesamiento de las observaciones
+    #
+    "cifbuild"
+    "odfingest"
+    #
+    ## Ejecutar tarea emproc
+    "emproc"
+    #
+    ## Ejecutar tarea epproc
+    "epproc"
+    #
+    # Filtrado de la lista de eventos.
+    #
+    "shopt -s nullglob; for file in *_*_EPN_*_ImagingEvts.ds; do if [ -f \"\$file\" ]; then cp \"\$file\" PN.fits >> \"\$LOG_FILE\"; fi; done"
+    "shopt -s nullglob; for file in *_*_EMOS1_*_ImagingEvts.ds; do if [ -f \"\$file\" ]; then cp \"\$file\" M1.fits >> \"\$LOG_FILE\"; fi; done"
+    "shopt -s nullglob; for file in *_*_EMOS2_*_ImagingEvts.ds; do if [ -f \"\$file\" ]; then cp \"\$file\" M2.fits >> \"\$LOG_FILE\"; fi; done"
+    ## Extraer una curva de luz de eventos singulares (con patrón “0”) a energías por encima de 10 keV para cada cámara (PN, MOS1, MOS2) para identificar los intervalos de alto background, usando la tarea `evselect` de SAS:
+    "evselect table=PN.fits withrateset=Y rateset=ratesPN.fits maketimecolumn=Y timebinsize=100 makeratecolumn=Y expression='#XMMEA_EP && (PI>10000&&PI<12000) && (PATTERN==0)'"
+    "evselect table=M1.fits withrateset=Y rateset=ratesM1.fits maketimecolumn=Y timebinsize=100 makeratecolumn=Y expression='#XMMEA_EM && (PI>10000) && (PATTERN==0)'"
+    "evselect table=M2.fits withrateset=Y rateset=ratesM2.fits maketimecolumn=Y timebinsize=100 makeratecolumn=Y expression='#XMMEA_EM && (PI>10000) && (PATTERN==0)'"
+    #
+    ## Usando la tarea `tabgtigen` se determinan los intervalos de tiempo en los que la curva de luz es baja y constante eligiendo un límite o *threshold* (en cuentas por segundo) para crear el archivo GTI, `EPICgti.fits`: 
+    "tabgtigen table=ratesPN.fits expression='RATE<=0.4' gtiset=PNgti.fits"
+    "tabgtigen table=ratesM1.fits expression='RATE<=0.35' gtiset=M1gti.fits"
+    "tabgtigen table=ratesM2.fits expression='RATE<=0.35' gtiset=M2gti.fits"
+    #
+    ##Por último, usamos nuevamente `evselect` para generar la lista de eventos filtrada, `EPICclean.fits`:
+    "evselect table=PN.fits withfilteredset=Y filteredset=PNclean.fits destruct=Y keepfilteroutput=T expression='#XMMEA_EP && gti(PNgti.fits,TIME) && (PI>150)'"
+    "evselect table=M1.fits withfilteredset=Y filteredset=M1clean.fits destruct=Y keepfilteroutput=T expression='#XMMEA_EM && gti(M1gti.fits,TIME) && (PI>150)'"
+    "evselect table=M2.fits withfilteredset=Y filteredset=M2clean.fits destruct=Y keepfilteroutput=T expression='#XMMEA_EM && gti(M2gti.fits,TIME) && (PI>150)'"
+)
 
-echo -e '\nInicializar HEASoft y SAS.' | tee -a  $LOG_FILE
-heainit
-sasinit | tee -a  $LOG_FILE
+echo "=======================" >> "$LOG_FILE"
+echo "XMM Pipeline - Log file" >> "$LOG_FILE"
+echo "=======================" >> "$LOG_FILE"
 
-# Almacenar la ruta hacia los archivos con las observaciones a procesar.
-indir=/home/shluna/Proyectos/rayosX/data/obs/xmm/$obsid/ODF
+printf "\nComandos ejecutados" >> "$LOG_FILE"
+printf "\n===================\n" >> "$LOG_FILE"
 
-# 1. Apuntar la variable `SAS_ODF` a la carpeta donde se encuentran las observaciones
-export SAS_ODF=${indir}
+for CMD in "${COMMANDS[@]}"; do
+    echo "- $CMD" >> "$LOG_FILE"
+done
 
-echo -e "\nLas observaciones se encuentran en: $SAS_ODF" | tee -a  $LOG_FILE
+# Ejecutar cada comando y registrar la salida
 
-echo -e "\nLos archivos generados por el reprocesamiento y el filtrado se guardan en: $outdir." | tee -a  $LOG_FILE
+echo -e "\n================================" >> "$LOG_FILE"
+echo -e "Log de ejecución de los comandos" >> "$LOG_FILE"
+echo -e "==================================\n" >> "$LOG_FILE"
 
-# 2. Luego, ejecutar la tareas cifbuild, la cual crea el archivo `cif.ccf`.
+# Ejecutar heainit y sasinit primero
+# Iniciar HEASoft
+echo -e "\nEjecutando: heainit" | tee -a "$LOG_FILE"
+eval "heainit" >> "$LOG_FILE" 2>&1
+echo -e "\nEjecutando: sasinit" | tee -a "$LOG_FILE"
+eval "sasinit" >> "$LOG_FILE" 2>&1
+echo -e "\n----------------------" >> "$LOG_FILE"
 
-echo -e '\nEjecutando tarea cifbuild.' | tee -a  $LOG_FILE
+# Reprocesamiento de las observaciones
+#
+## Ejecutar la tarea cifbuild, la cual crea el archivo `cif.ccf`.
+echo -e "\nEjecutando: cifbuild" | tee -a "$LOG_FILE"
+eval "cifbuild" >> "$LOG_FILE" 2>&1
+# Apuntar la variable SAS_CCF al archivo cif.ccf generado por la tarea cifbuild
+export SAS_CCF="cif.ccf"
+echo -e "\nArchivo SAS_CCF configurado: $SAS_CCF" | tee -a "$LOG_FILE"
+echo -e "\n----------------------" >> "$LOG_FILE"
 
-cifbuild | tee -a  $LOG_FILE
+## Ejecutar tarea odfingest
+echo -e "\nEjecutando: odfingest" | tee -a "$LOG_FILE"
+eval "odfingest" >> "$LOG_FILE" 2>&1
 
-# 3. Apuntar la variable `SAS_CCF` a este último archivo:`export SAS_CCF=cif.ccf`.
-export SAS_CCF=ccf.cif
+# Buscar el archivo SUM.SAS
+SAS_ODF=$(find . -maxdepth 1 -type f -name "*SUM.SAS" | head -n 1)
 
-# 4. Ejecutar la tarea `odfingest` para actualizar el archivo de resumen `*SUM.SAS`.
-echo -e '\nEjecutando tarea odfingest.' | tee -a  $LOG_FILE
+# Verificar si se encontró el archivo
+if [ -z "$SAS_ODF" ]; then
+    echo -e "\nNo se encontró el archivo de resumen." | tee -a "$LOG_FILE"
+fi
 
-odfingest | tee -a  $LOG_FILE
+# Exportar la variable SAS_ODF
+echo -e "\nArchivo de resumen encontrado: $SAS_ODF" | tee -a "$LOG_FILE"
 
-# 5. Apuntar la variable `SAS_ODF` al archivo de resumen actualizado: `export SAS_ODF=*SUM.SAS`.
-export SAS_ODF=`ls | grep *SUM.SAS`
+# Apuntar la variable SAS_ODF al archivo de resumen generado por odfingest.
+export SAS_ODF="${outdir}/$SAS_ODF"
+echo -e "\nArchivo SAS_ODF configurado: $SAS_ODF" | tee -a "$LOG_FILE"
+echo -e"\n----------------------" >> "$LOG_FILE"
 
-echo -e "\nArchivo de resumen actualizado: $SAS_ODF" | tee -a  $LOG_FILE
+# Encontrar el índice del comando "emproc"
+start_index=$(echo "${COMMANDS[@]}" | tr ' ' '\n' | grep -n -m 1 "emproc" | cut -d ':' -f 1)
 
-#6. Por último, ejecutar las tareas `emproc`y `epproc` para reprocesar las observaciones de los detectores EPIC-MOS y EPIC-pn, respectivamente.
+# Verificar si el comando "emproc" fue encontrado
+if [ -z "$start_index" ]; then
+    echo -e "\nNo se encontró el comando 'emproc' en la lista." | tee -a "$LOG_FILE"
+fi
 
-echo -e '\nEjecutando tarea emproc.' | tee -a  $LOG_FILE
+# Ejecutar los comandos desde "epproc" en adelante
+for ((i=$start_index; i<${#COMMANDS[@]}; i++)); do
+    CMD="${COMMANDS[$i]}"
+    echo -e "\nEjecutando: $CMD" | tee -a "$LOG_FILE"
+    eval "$CMD" >> "$LOG_FILE" 2>&1
+    echo -e "\n----------------------" >> "$LOG_FILE"
+done
 
-emproc | tee -a  $LOG_FILE
+echo -e "\n¡Listo!" >> "$LOG_FILE"
 
-echo -e '\nEjecutando tarea epproc.' | tee -a  $LOG_FILE
+echo -e "\nEjecución completada. Ver $LOG_FILE para detalles.\n"
 
-epproc | tee -a  $LOG_FILE
-
-## Filtrado de la lista de eventos.
-
-# Antes de comenzar conviene hacer una copia de seguridad de las listas eventos originales detectados por cada cámara. Estos están guardado en los archivos que finalizan `*Evts.ds`:
-cp *_*_EPN_*_ImagingEvts.ds PN.fits
-cp *_*_EMOS1_*_ImagingEvts.ds M1.fits
-cp *_*_EMOS2_*_ImagingEvts.ds M2.fits
-
-echo -e "\nIdentificando los intervalos de alto background." | tee -a  $LOG_FILE
-
-# 1. Extraer una curva de luz de eventos singulares (con patrón “0”) a energías por encima de 10 keV para cada cámara (PN, MOS1, MOS2) para identificar los intervalos de alto background, usando la tarea `evselect` de SAS:
-evselect table=PN.fits withrateset=Y rateset=ratesPN.fits maketimecolumn=Y timebinsize=100 makeratecolumn=Y expression='#XMMEA_EP && (PI>10000&&PI<12000) && (PATTERN==0)' | tee -a  $LOG_FILE
-evselect table=M1.fits withrateset=Y rateset=ratesM1.fits maketimecolumn=Y timebinsize=100 makeratecolumn=Y expression='#XMMEA_EM && (PI>10000) && (PATTERN==0)' | tee -a  $LOG_FILE
-evselect table=M2.fits withrateset=Y rateset=ratesM2.fits maketimecolumn=Y timebinsize=100 makeratecolumn=Y expression='#XMMEA_EM && (PI>10000) && (PATTERN==0)' | tee -a  $LOG_FILE
-
-echo -e "\nDeterminando los intervalos de tiempo en los que la curva de luz es baja y constante" | tee -a  $LOG_FILE
-
-# 2. Usando la tarea `tabgtigen` se determinan los intervalos de tiempo en los que la curva de luz es baja y constante eligiendo un límite o *threshold* (en cuentas por segundo) para crear el archivo GTI, `EPICgti.fits`: 
-tabgtigen table=ratesPN.fits expression='RATE<=0.4' gtiset=PNgti.fits | tee -a  $LOG_FILE
-tabgtigen table=ratesM1.fits expression='RATE<=0.35' gtiset=M1gti.fits | tee -a  $LOG_FILE
-tabgtigen table=ratesM2.fits expression='RATE<=0.35' gtiset=M2gti.fits | tee -a  $LOG_FILE
-
-echo -e "\nGenerando la lista de eventos filtrada." | tee -a  $LOG_FILE
-
-# 3. Por último, usamos nuevamente `evselect` para generar la lista de eventos filtrada, `EPICclean.fits`:
-evselect table=PN.fits withfilteredset=Y filteredset=PNclean.fits destruct=Y keepfilteroutput=T expression='#XMMEA_EP && gti(PNgti.fits,TIME) && (PI>150)' | tee -a  $LOG_FILE
-evselect table=M1.fits withfilteredset=Y filteredset=M1clean.fits destruct=Y keepfilteroutput=T expression='#XMMEA_EM && gti(M1gti.fits,TIME) && (PI>150)' | tee -a  $LOG_FILE
-evselect table=M2.fits withfilteredset=Y filteredset=M2clean.fits destruct=Y keepfilteroutput=T expression='#XMMEA_EM && gti(M2gti.fits,TIME) && (PI>150)' | tee -a  $LOG_FILE
-
-echo -e '\n¡Listo!' | tee -a  $LOG_FILE
-echo -e '\nAbrir los archivos PNclean.fits, M1clean.fits y M2clean.fits con ds9 y seleccionar las regiones correspondientes a la fuente y al background para continuar con la generación de productos (espectro y curvas de luz).\n Guardar las regiones usando las coordenadas físicas.\n'
+echo -e "\nAbrir los archivos PNclean.fits, M1clean.fits y M2clean.fits con ds9 y seleccionar las regiones correspondientes a la fuente y al background para continuar con la generación de productos (espectro y curvas de luz). Guardar las regiones usando las coordenadas físicas.\n" | tee -a  "$LOG_FILE"
 
 cd $RUTA_ESPERADA
