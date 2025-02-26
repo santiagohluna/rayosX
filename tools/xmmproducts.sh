6b2b250e2db3687f582d5720d8cf929f1b76c3a5
Santiago Luna <santiago.luna@unahur.edu.ar>
2025-02-26 14:53:28 -0300

    Se movió el bloque de código que crea el archivo de salidapara evitar que este se cree en dos lugares diferentes, entre otras correcciones

--- tools/xmmproducts.sh
+++ tools/xmmproducts.sh
@@ -47,33 +47,6 @@ fi
 cd $obsid
 echo -e "\nSe cambió el directorio de trabajo a $(pwd)"
 
-# Cadena para el nombre de la carpeta de salida y log.
-STAMP=$(date +'d%Y%m%d_t%H%M%S')
-
-# Crear archivo de log
-LOG_FILE="xmmproducts_"$STAMP".log"
-
-printf '\n=======================' >> $LOG_FILE
-printf '\nXMM Products - Log file' >> $LOG_FILE
-printf '\n=======================' >> $LOG_FILE
-
-# 1. En primer lugar, se deben inicializar las variables de entorno SAS_ODF y SAS_CCF:
-
-# Almacenar la ruta hacia los archivos con las observaciones a procesar.
-indir=/home/shluna/Proyectos/rayosX/data/xmm/$obsid/ODF
-
-# Apuntar la variable de entorne SAS_CCF al archivo 'ccf.cif'.
-SAS_CCF=$(find $indir -type f -name "ccf.cif")
-export SAS_CCF=$SAS_CCF
-echo -e "\nVariable de entorno SAS_CCF configurada: $SAS_CCF" | tee -a "$LOG_FILE"
-echo -e "\n----------------------" >> "$LOG_FILE"
-
-# Apuntar la variable SAS_ODF al archivo de resumen generado por odfingest.
-SAS_ODF=$(find $indir -type f -name "*SUM.SAS")
-export SAS_ODF=$SAS_ODF
-echo -e "\nVariable de entorno SAS_ODF configurada: $SAS_ODF" | tee -a "$LOG_FILE"
-echo -e "\n----------------------" >> "$LOG_FILE"
-
 # Buscar los directorios en la carpeta donde se guardan las reducciones y almacenarlos en un array:
 readarray x < <(find . -type d -name "PPO_*")
 
@@ -92,7 +65,7 @@ else
     indir=$(printf %s ${x[0]})
 fi
 
-printf "\nEl directorio seleccionado es: %s" "$indir"
+printf "\nEl directorio seleccionado es: %s" "$indir"{regfiles[$id]}
 
 readarray regfiles < <(find . -type f -name "*.reg")
 
@@ -107,22 +80,22 @@ cd $indir
 
 printf "\nIngrese el nombre del archivo de región de la fuente para la cámara EPIC-PN: "
 read -e regfile
-params[0]=${regfiles[$id]}
+params[0]=$regfile
 printf "\nIngrese el nombre del archivo de región de la fuente para la cámara EPIC-MOS1: "
 read -e regfile
-params[1]=${regfiles[$id]}
+params[1]=$regfile
 printf "\nIngrese el nombre del archivo de región de la fuente para la cámara EPIC-MOS2: "
 read -e regfile
-params[2]=${regfiles[$id]}
+params[2]=$regfile
 printf "\nIngrese el nombre del archivo de región del background para la cámara EPIC-PN: "
 read -e regfile
-params[3]=${regfiles[$id]}
+params[3]=$regfile
 printf "\nIngrese el nombre del archivo de región del background para la cámara EPIC-MOS1: "
 read -e regfile
-params[4]=${regfiles[$id]}
+params[4]=$regfile
 printf "\nIngrese el nombre del archivo de región del background para la cámara EPIC-MOS2: "
 read -e regfile
-params[5]=${regfiles[$id]}
+params[5]=$regfile
 
 printf "\nLos archivos de regiones son:\n"
 printf "\n1. Fuente (cámara EPIC-PN): %s" "${params[0]}"
@@ -160,6 +133,9 @@ done
 
 cd ..
 
+# Cadena para el nombre de la carpeta de salida y log.
+STAMP=$(date +'d%Y%m%d_t%H%M%S')
+
 # Crear la carpeta donde se van a almacenar los archivos que resultan de la reducción.
 
 outdir="products_$STAMP"
@@ -170,49 +146,73 @@ mkdir -p $(echo $outdir)
 
 cd $outdir
 
+# Crear archivo de log
+LOG_FILE="xmmproducts_"$STAMP".log"
+
+printf '\n=======================' >> $LOG_FILE
+printf '\nXMM Products - Log file' >> $LOG_FILE
+printf '\n=======================' >> $LOG_FILE
+
+# 1. En primer lugar, se deben inicializar las variables de entorno SAS_ODF y SAS_CCF:
+
+# Almacenar la ruta hacia los archivos con las observaciones a procesar.
+odfdir=/home/shluna/Proyectos/rayosX/data/xmm/$obsid/ODF
+
+# Apuntar la variable de entorne SAS_CCF al archivo 'ccf.cif'.
+SAS_CCF=$(find $odfdir -type f -name "ccf.cif")
+export SAS_CCF=$SAS_CCF
+echo -e "\nVariable de entorno SAS_CCF configurada: $SAS_CCF" | tee -a "$LOG_FILE"
+echo -e "\n----------------------" >> "$LOG_FILE"
+
+# Apuntar la variable SAS_ODF al archivo de resumen generado por odfingest.
+SAS_ODF=$(find $odfdir -type f -name "*SUM.SAS")
+export SAS_ODF=$SAS_ODF
+echo -e "\nVariable de entorno SAS_ODF configurada: $SAS_ODF" | tee -a "$LOG_FILE"
+echo -e "\n----------------------" >> "$LOG_FILE"
+
 # Arreglo con los comando a ejecutar:
 COMMANDS=(
     # 2. Extraer la curva de luz de la fuente usando la región elegida e incluyendo una selección de eventos de calidad apropiada para la curva de luz y un agrupamiento de, por ejemplo, 5 segundos.
-    "evselect table=$indir/PNclean.fits energycolumn=PI expression='(FLAG==0) && (PATTERN<=4) && (PI in [300:10000]) && ((X,Y) IN "${params[0]}")' withrateset=yes rateset=PN_src_raw.lc timebinsize=5 maketimecolumn=yes makeratecolumn=yes"
-    "evselect table=$indir/M1clean.fits energycolumn=PI expression='(FLAG==0) && (PATTERN<=12) && (PI in [300:10000]) && ((X,Y) IN "${params[1]}")' withrateset=yes rateset=$indir/M1_src_raw.lc timebinsize=5 maketimecolumn=yes makeratecolumn=yes"
-    "evselect table=$indir/M2clean.fits energycolumn=PI expression='(FLAG==0) && (PATTERN<=12) && (PI in [300:10000]) && ((X,Y) IN "${params[2]}")' withrateset=yes rateset=$indir/M2_src_raw.lc timebinsize=5 maketimecolumn=yes makeratecolumn=yes" 
+    "evselect table=$indir/PNclean.evts energycolumn=PI expression='(FLAG==0) && (PATTERN<=4) && (PI in [300:10000]) && ((X,Y) IN "${params[0]}")' withrateset=yes rateset=PN_src_raw.lc timebinsize=5 maketimecolumn=yes makeratecolumn=yes"
+    "evselect table=$indir/M1clean.evts energycolumn=PI expression='(FLAG==0) && (PATTERN<=12) && (PI in [300:10000]) && ((X,Y) IN "${params[1]}")' withrateset=yes rateset=$indir/M1_src_raw.lc timebinsize=5 maketimecolumn=yes makeratecolumn=yes"
+    "evselect table=$indir/M2clean.evts energycolumn=PI expression='(FLAG==0) && (PATTERN<=12) && (PI in [300:10000]) && ((X,Y) IN "${params[2]}")' withrateset=yes rateset=$indir/M2_src_raw.lc timebinsize=5 maketimecolumn=yes makeratecolumn=yes" 
     # 3. Extraer la curva de luz del fondo, usando las mismas expresiones que para la fuente:
-    "evselect table=$indir/PNclean.fits energycolumn=PI expression='(FLAG==0) && (PATTERN<=4) && (PI in [300:10000]) && ((X,Y) IN "${params[3]}")' withrateset=yes rateset=PN_bkg_raw.lc timebinsize=5 maketimecolumn=yes makeratecolumn=yes"
-    "evselect table=$indir/M1clean.fits energycolumn=PI expression='(FLAG==0) && (PATTERN<=12) && (PI in [300:10000]) && ((X,Y) IN "${params[4]}")' withrateset=yes rateset=M1_bkg_raw.lc timebinsize=5 maketimecolumn=yes makeratecolumn=yes"
-    "evselect table=$indir/M2clean.fits energycolumn=PI expression='(FLAG==0) && (PATTERN<=12) && (PI in [300:10000]) && ((X,Y) IN "${params[5]}")' withrateset=yes rateset=M2_bkg_raw.lc timebinsize=5 maketimecolumn=yes makeratecolumn=yes"
+    "evselect table=$indir/PNclean.evt energycolumn=PI expression='(FLAG==0) && (PATTERN<=4) && (PI in [300:10000]) && ((X,Y) IN "${params[3]}")' withrateset=yes rateset=PN_bkg_raw.lc timebinsize=5 maketimecolumn=yes makeratecolumn=yes"
+    "evselect table=$indir/M1clean.evts energycolumn=PI expression='(FLAG==0) && (PATTERN<=12) && (PI in [300:10000]) && ((X,Y) IN "${params[4]}")' withrateset=yes rateset=M1_bkg_raw.lc timebinsize=5 maketimecolumn=yes makeratecolumn=yes"
+    "evselect table=$indir/M2clean.evts energycolumn=PI expression='(FLAG==0) && (PATTERN<=12) && (PI in [300:10000]) && ((X,Y) IN "${params[5]}")' withrateset=yes rateset=M2_bkg_raw.lc timebinsize=5 maketimecolumn=yes makeratecolumn=yes"
     # 4. Corrección de las curvas de luz.
-    "epiclccorr srctslist=PN_src_raw.lc eventlist=$indir/PNclean.fits outset=PN_lccorr.lc bkgtslist=PN_bkg_raw.lc withbkgset=yes applyabsolutecorrections=yes"
-    "epiclccorr srctslist=$indir/M1_src_raw.lc eventlist=$indir/M1clean.fits outset=M1_lccorr.lc bkgtslist=M1_bkg_raw.lc withbkgset=yes applyabsolutecorrections=yes"
-    "epiclccorr srctslist=$indir/M2_src_raw.lc eventlist=$indir/M2clean.fits outset=M2_lccorr.lc bkgtslist=M2_bkg_raw.lc withbkgset=yes applyabsolutecorrections=yes"
+    "epiclccorr srctslist=PN_src_raw.lc eventlist=$indir/PNclean.evt outset=PN_lccorr.lc bkgtslist=PN_bkg_raw.lc withbkgset=yes applyabsolutecorrections=yes"
+    "epiclccorr srctslist=$indir/M1_src_raw.lc eventlist=$indir/M1clean.evts outset=M1_lccorr.lc bkgtslist=M1_bkg_raw.lc withbkgset=yes applyabsolutecorrections=yes"
+    "epiclccorr srctslist=$indir/M2_src_raw.lc eventlist=$indir/M2clean.evts outset=M2_lccorr.lc bkgtslist=M2_bkg_raw.lc withbkgset=yes applyabsolutecorrections=yes"
     #
     # Obtención de los espectros
     #
     # 1. Extraer el espectro de la fuente.
-    "evselect table=$indir/PNclean.fits withspectrumset=yes spectrumset=PN_src_spectrum.fits energycolumn=PI spectralbinsize=5 withspecranges=yes specchannelmin=0 specchannelmax=20479 expression='(FLAG==0) && (PATTERN<=4) && ((X,Y) IN "${params[0]}")'"
-    "evselect table=$indir/M1clean.fits withspectrumset=yes spectrumset=M1_src_spectrum.fits energycolumn=PI spectralbinsize=5 withspecranges=yes specchannelmin=0 specchannelmax=11999 expression='(FLAG==0) && (PATTERN<=12) && ((X,Y) IN "${params[1]}")'"
-    "evselect table=$indir/M2clean.fits withspectrumset=yes spectrumset=M2_src_spectrum.fits energycolumn=PI spectralbinsize=5 withspecranges=yes specchannelmin=0 specchannelmax=11999 expression='(FLAG==0) && (PATTERN<=12) && ((X,Y) IN "${params[2]}")'"
+    "evselect table=$indir/PNclean.evt withspectrumset=yes spectrumset=PN_src_spectrum.fits energycolumn=PI spectralbinsize=5 withspecranges=yes specchannelmin=0 specchannelmax=20479 expression='(FLAG==0) && (PATTERN<=4) && ((X,Y) IN ${params[0]})'"
+    "evselect table=$indir/M1clean.evts withspectrumset=yes spectrumset=M1_src_spectrum.fits energycolumn=PI spectralbinsize=5 withspecranges=yes specchannelmin=0 specchannelmax=11999 expression='(FLAG==0) && (PATTERN<=12) && ((X,Y) IN ${params[1]})'"
+    "evselect table=$indir/M2clean.evts withspectrumset=yes spectrumset=M2_src_spectrum.fits energycolumn=PI spectralbinsize=5 withspecranges=yes specchannelmin=0 specchannelmax=11999 expression='(FLAG==0) && (PATTERN<=12) && ((X,Y) IN ${params[2]})'"
     # 2. Extraer el espectro del fondo:
-    "evselect table=$indir/PNclean.fits withspectrumset=yes spectrumset=PN_src_bkg_spectrum.pha energycolumn=PI spectralbinsize=5 withspecranges=yes specchannelmin=0 specchannelmax=20479 expression='(FLAG==0) && (PATTERN<=4) && ((X,Y) IN "${params[3]}")'"
-    "evselect table=$indir/M1clean.fits withspectrumset=yes spectrumset=M1_src_bkg_spectrum.pha energycolumn=PI spectralbinsize=5 withspecranges=yes specchannelmin=0 specchannelmax=11999 expression='(FLAG==0) && (PATTERN<=12) && ((X,Y) IN "${params[4]}")'"
-    "evselect table=$indir/M2clean.fits withspectrumset=yes spectrumset=M2_src_bkg_spectrum.pha energycolumn=PI spectralbinsize=5 withspecranges=yes specchannelmin=0 specchannelmax=11999 expression='(FLAG==0) && (PATTERN<=12) && ((X,Y) IN "${params[5]}")'"
+    "evselect table=$indir/PNclean.evt withspectrumset=yes spectrumset=PN_src_bkg_spectrum.pha energycolumn=PI spectralbinsize=5 withspecranges=yes specchannelmin=0 specchannelmax=20479 expression='(FLAG==0) && (PATTERN<=4) && ((X,Y) IN ${params[3]})'"
+    "evselect table=$indir/M1clean.evts withspectrumset=yes spectrumset=M1_src_bkg_spectrum.pha energycolumn=PI spectralbinsize=5 withspecranges=yes specchannelmin=0 specchannelmax=11999 expression='(FLAG==0) && (PATTERN<=12) && ((X,Y) IN ${params[4]})'"
+    "evselect table=$indir/M2clean.evts withspectrumset=yes spectrumset=M2_src_bkg_spectrum.pha energycolumn=PI spectralbinsize=5 withspecranges=yes specchannelmin=0 specchannelmax=11999 expression='(FLAG==0) && (PATTERN<=12) && ((X,Y) IN ${params[5]})'"
     # 3. Calcular el area de las regiones usadas para extraer la fuente y el fondo necesarias para pesar correctamente sus flujos mutuos.
     ## Para EPIC-pn:
-    "backscale spectrumset=PN_src_spectrum.fits badpixlocation=$indir/PNclean.fits"
-    "backscale spectrumset=PN_src_bkg_spectrum.pha badpixlocation=$indir/PNclean.fits"
+    "backscale spectrumset=PN_src_spectrum.fits badpixlocation=$indir/PNclean.evt"
+    "backscale spectrumset=PN_src_bkg_spectrum.pha badpixlocation=$indir/PNclean.evt"
     ## Para EPIC-MOS 1:
-    "backscale spectrumset=M1_src_spectrum.fits badpixlocation=$indir/M1clean.fits"
-    "backscale spectrumset=M1_src_bkg_spectrum.pha badpixlocation=$indir/M1clean.fits"
+    "backscale spectrumset=M1_src_spectrum.fits badpixlocation=$indir/M1clean.evts"
+    "backscale spectrumset=M1_src_bkg_spectrum.pha badpixlocation=$indir/M1clean.evts"
     ## Para EPIC-MOS 2:
-    "backscale spectrumset=M2_src_spectrum.fits badpixlocation=$indir/M2clean.fits"
-    "backscale spectrumset=M2_src_bkg_spectrum.pha badpixlocation=$indir/M2clean.fits"
+    "backscale spectrumset=M2_src_spectrum.fits badpixlocation=$indir/M2clean.evts"
+    "backscale spectrumset=M2_src_bkg_spectrum.pha badpixlocation=$indir/M2clean.evts"
     # 4. Usar la tarea rmfgen para crear una matriz de redistribución para el espectro extraído (esto puede llevar más de 30 minutos en computadoras pequeñas):
     "rmfgen spectrumset=PN_src_spectrum.fits rmfset=PN_src.rmf"
     "rmfgen spectrumset=M1_src_spectrum.fits rmfset=M1_src.rmf"
     "rmfgen spectrumset=M2_src_spectrum.fits rmfset=M2_src.rmf"
     # 5. Generar la matriz auxiliar. Para fuentes puntuales usar extendedsource=no detmaptype=psf. Para fuentes extendidas usar extendedsource=yes detmaptype=flat o bien generar un mapa de exposición con la tarea expmap).
-    "arfgen spectrumset=PN_src_spectrum.fits arfset=PN_src.arf withrmfset=yes rmfset=PN_src.rmf badpixlocation=$indir/PNclean.fits extendedsource=no detmaptype=psf"
-    "arfgen spectrumset=M1_src_spectrum.fits arfset=M1_src.arf withrmfset=yes rmfset=M1_src.rmf badpixlocation=$indir/M1clean.fits extendedsource=no detmaptype=psf"
-    "arfgen spectrumset=M2_src_spectrum.fits arfset=M2_src.arf withrmfset=yes rmfset=M2_src.rmf badpixlocation=$indir/M2clean.fits extendedsource=no detmaptype=psf"
+    "arfgen spectrumset=PN_src_spectrum.fits arfset=PN_src.arf withrmfset=yes rmfset=PN_src.rmf badpixlocation=$indir/PNclean.evt extendedsource=no detmaptype=psf"
+    "arfgen spectrumset=M1_src_spectrum.fits arfset=M1_src.arf withrmfset=yes rmfset=M1_src.rmf badpixlocation=$indir/M1clean.evts extendedsource=no detmaptype=psf"
+    "arfgen spectrumset=M2_src_spectrum.fits arfset=M2_src.arf withrmfset=yes rmfset=M2_src.rmf badpixlocation=$indir/M2clean.evts extendedsource=no detmaptype=psf"
     # 6. Reagrupar el espectro y vincularlo a los archivos asociados tales como el espectro del fondo y las matrices (RMF y ARF). En el ejemplo reagrupamos a un mínimo de 16 cuentas por canal asegurando que el reagrupamiento no exceda un factor 3 en la pérdida de resolución:
     "specgroup spectrumset=PN_src_spectrum.fits mincounts=16 oversample=3 rmfset=PN_src.rmf arfset=PN_src.arf backgndset=PN_src_bkg_spectrum.pha groupedset=PN_src_grp.pha"
     "specgroup spectrumset=M1_src_spectrum.fits mincounts=16 oversample=3 rmfset=M1_src.rmf arfset=M1_src.arf backgndset=M1_src_bkg_spectrum.pha groupedset=M1_src_grp.pha"
